8c3074f361b04bd234e9222859878f867af24a7f
Deniz Esen <esen@other.ch>
2022-09-30 13:14:20 +0200

    v 0.28 start

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM.
 
-[assembly: AssemblyFileVersion("0.27.0.0")]
-[assembly: AssemblyVersion("0.27.0.0")]
+[assembly: AssemblyFileVersion("0.28.0.0")]
+[assembly: AssemblyVersion("0.28.0.0")]
