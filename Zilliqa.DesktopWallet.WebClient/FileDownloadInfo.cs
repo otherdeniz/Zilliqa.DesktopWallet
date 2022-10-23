64ac541c8b40eb4ebd46c95be174f63623238465
Deniz Mobile <esen@other.ch>
2022-10-23 20:40:05 +0200

    web client work...

---
+++ Zilliqa.DesktopWallet.WebClient/FileDownloadInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace Zilliqa.DesktopWallet.WebClient
+{
+    public class FileDownloadInfo
+    {
+        private readonly Stream _sourceStream;
+
+        public FileDownloadInfo(string filePath, Stream sourceStream)
+        {
+            _sourceStream = sourceStream;
+            FilePath = filePath;
+        }
+
+        public string FilePath { get; }
+
+    }
+}
--- Zilliqa.DesktopWallet.WebClient/WalletWebClient.cs
+++ Zilliqa.DesktopWallet.WebClient/WalletWebClient.cs
@@ -1,4 +1,8 @@
-﻿namespace Zilliqa.DesktopWallet.WebClient
+﻿using Newtonsoft.Json;
+using RestSharp;
+using Zilliqa.DesktopWallet.WebContract;
+
+namespace Zilliqa.DesktopWallet.WebClient
 {
     public class WalletWebClient
     {
@@ -8,6 +12,31 @@
         }
 
         public string ServerUrl { get; }
+
+        public SnapshotInfo? GetSnapshotInfo()
+        {
+            using (var client = GetClient())
+            {
+                var request = new RestRequest("snapshot");
+                var response = client.Execute(request);
+                if (response.IsSuccessful && !string.IsNullOrEmpty(response.Content))
+                {
+                    return JsonConvert.DeserializeObject<SnapshotInfo>(response.Content);
+                }
+            }
+            return null;
+        }
+
+        public FileDownloadInfo? DownloadSnapshot(string snapshotId, string destinationFilePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        private RestClient GetClient()
+        {
+            var client = new RestClient($"{ServerUrl}/api");
+            return client;
+        }
 
     }
 }
